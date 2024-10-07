fbd7183ccad7234bf7ab31604805a02e6593a102
Anurag <anurag3487@gmail.com>
2024-10-07 21:44:32 +0530

    GridView with checkbox columns

---
+++ WebFormsSample/WebFormsSample/Sample.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="Sample Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Sample.aspx.cs" Inherits="WebFormsSample.Sample" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <h1>Sample Page</h1>
+        <asp:GridView runat="server" ID="gridViewEmployee" AutoGenerateColumns="false" OnRowDataBound="gridViewEmployee_RowDataBound" DataKeyNames="ID">
+            <Columns>
+                <asp:TemplateField ItemStyle-Width="100">
+                    <HeaderTemplate>
+                        <asp:CheckBox runat="server" ID="chkHeader" />
+                    </HeaderTemplate>
+                    <ItemTemplate>
+                        <asp:CheckBox runat="server" ID="chkRow" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="ID" HeaderText="Id" ItemStyle-Width="100" />
+                <asp:BoundField DataField="Name" HeaderText="Name" ItemStyle-Width="100" />
+                <asp:BoundField DataField="City" HeaderText="City" ItemStyle-Width="100" />
+            </Columns>
+        </asp:GridView>
+
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit Selected Rows" OnClick="btnSubmit_Click" />
+
+    </main>
+</asp:Content>
---
+++ WebFormsSample/WebFormsSample/Sample.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsSample
+{
+    public partial class Sample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        protected void gridViewEmployee_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            foreach(GridViewRow row in gridViewEmployee.Rows)
+            {
+                var checkBox = (CheckBox)row.FindControl("chkRow");
+                if(checkBox != null && checkBox.Checked)
+                {
+                    string id = gridViewEmployee.DataKeys[row.RowIndex].Value.ToString();
+                    string name = row.Cells[2].Text;
+                    string city = row.Cells[3].Text;
+
+                    Response.Write($"Selected Rows Data: ID = {id}, Name = {name}, City = {city} <br/>");
+                }
+            }
+        }
+
+        public void BindGrid()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("City");
+
+            dt.Rows.Add("101", "John1", "NY1");
+            dt.Rows.Add("102", "John2", "NY2");
+            dt.Rows.Add("103", "John3", "NY3");
+            dt.Rows.Add("104", "John4", "NY4");
+            dt.Rows.Add("105", "John5", "NY5");
+            dt.Rows.Add("106", "John6", "NY6");
+            dt.Rows.Add("107", "John7", "NY7");
+
+            gridViewEmployee.DataSource = dt;
+            gridViewEmployee.DataBind();
+        }
+    }
+}
